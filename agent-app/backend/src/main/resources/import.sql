d40e3aa025d602905088775a807fce67991aaa54
SDragovic99 <73397006+SDragovic99@users.noreply.github.com>
2022-05-25 23:32:47 +0200

    feat: Add endpoints for testing access control

--- agent-app/backend/src/main/resources/import.sql
+++ agent-app/backend/src/main/resources/import.sql
@@ -2,3 +2,5 @@ insert into role (id, name) values ('2273ea92-dad3-11ec-9d64-0242ac120002', 'ROL
 insert into role (id, name) values ('3d89303a-dad3-11ec-9d64-0242ac120002', 'ROLE_ADMIN');
 insert into role (id, name) values ('4492ab54-dad3-11ec-9d64-0242ac120002', 'ROLE_COMPANY_OWNER');
 insert into users (id, email, password, role_id, is_enabled, is_deleted) values ('58b17b60-dad3-11ec-9d64-0242ac120002', 'admin@gmail.com', '$2a$10$t5B4Vu20.u//zjDP2IU4kOR49tqzbUo9WRVQ50rV1Og3FxBsioG2C', '3d89303a-dad3-11ec-9d64-0242ac120002', true, false);
+insert into users (id, email, password, role_id, is_enabled, is_deleted) values ('b6588e23-c3ac-45c5-b7de-35cf173d065a', 'user@gmail.com', '$2a$10$t5B4Vu20.u//zjDP2IU4kOR49tqzbUo9WRVQ50rV1Og3FxBsioG2C', '2273ea92-dad3-11ec-9d64-0242ac120002', true, false);
+insert into users (id, email, password, role_id, is_enabled, is_deleted) values ('a05038f5-4f38-4b0b-8402-abeb66ba4031', 'company-owner@gmail.com', '$2a$10$t5B4Vu20.u//zjDP2IU4kOR49tqzbUo9WRVQ50rV1Og3FxBsioG2C', '4492ab54-dad3-11ec-9d64-0242ac120002', true, false);
